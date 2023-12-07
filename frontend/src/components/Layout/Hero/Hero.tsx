986dd99cdb3bb3cc39d52e3ab33e522f08d9c180
Sebastien <34372773+SebastienNovak@users.noreply.github.com>
2023-12-07 18:02:35 +1100

    frontend: feat: added hero banner

--- frontend/src/components/Layout/Hero/Hero.tsx
+++ frontend/src/components/Layout/Hero/Hero.tsx
@@ -1,18 +1,20 @@
-import './hero.scss'; // Your styling file
+import './hero.scss'; // Ensure the path is correct
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 
-const HeroSection = () => {
+const Hero = () => {
     return (
         <div className="hero">
             <h1>Mint Mate: Your NFT Ticket Hub</h1>
             <p>Mint your moment. Own your experience.</p>
-            <button className="signup-button">Sign Up</button>
-            <button className="login-button">
-                <FontAwesomeIcon icon={faUser} /> Log In
-            </button>
+            <div className="hero-buttons">
+                <button className="signup-button">Sign Up</button>
+                <button className="login-button">
+                    <FontAwesomeIcon icon={faUser} /> Log In
+                </button>
+            </div>
         </div>
     );
 };
 
-export default HeroSection;
+export default Hero;
